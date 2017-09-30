cc5733b1a8623504ec4b2dba78909689eba5a33c
Martin Svärd <martinsvard@me.com>
2017-09-30 10:24:58 +0200

    The triangle the mouse hovers is now marked

--- src/grid-interaction.js
+++ src/grid-interaction.js
@@ -8,22 +8,22 @@ const CONTAINER = document.querySelector('.graphics-wrapper')
 export default class GridInteraction {
   constructor () {
     this.gh = new GraphicsHandler(CONTAINER)
-
-    this.gh.strokeStyle = '#ffdddd'
-
   }
 
   render () {
     const gPos = gtr.toGlobal(MouseHandler.position().x, MouseHandler.position().y)
 
-    const a1 = this.getDiagonal(gPos, ALPHA)
-    const a2 = this.getDiagonal(gPos, -ALPHA)
+    const a1 = this.getDiagonalOffset(gPos, ALPHA)
+    const a2 = this.getDiagonalOffset(gPos, -ALPHA)
 
     const poly = []
-    poly.push(this.getPoint(a1, a2))
-    poly.push(this.getPoint(a1 + 1, a2))
-    poly.push(this.getPoint(a1 + 1, a2 + 1))
-    poly.push(this.getPoint(a1, a2 + 1))
+    poly.push(this.getIntersection(a1, a2))
+    poly.push(this.getIntersection(a1 + 1, a2 + 1))
+    if (gPos.x > poly[0].x) {
+      poly.push(this.getIntersection(a1, a2 + 1))
+    } else {
+      poly.push(this.getIntersection(a1 + 1, a2))
+    }
 
     this.gh.clearCanvas()
     this.gh.fillStyle = 'rgba(0, 0, 0, 0.2)'
@@ -31,34 +31,17 @@ export default class GridInteraction {
 
     this.gh.fillStyle = '#222222'
     this.gh.writeText(`zoom: ${gtr.zoom}`, 20, 20)
-    this.gh.writeText(`x: ${gPos.x}`, 100, 20)
-    this.gh.writeText(`y: ${gPos.y}`, 180, 20)
-    this.gh.writeText(`d1: ${a1}`, 20, 45)
-    this.gh.writeText(`d2: ${a2}`, 20, 80)
+    this.gh.writeText(`d1: ${a1}`, 100, 20)
+    this.gh.writeText(`d2: ${a2}`, 180, 20)
   }
 
-  getDiagonal (p, angle) {
+  getDiagonalOffset (p, angle) {
     return Math.floor(p.y - angle * p.x)
   }
 
-  getPoint (a1, a2) {
+  getIntersection (a1, a2) {
     const x = (a2 - a1) / (2 * ALPHA)
     const y = a1 + ALPHA * x
-
     return {x: x, y: y}
   }
 }
-
-/*
-    y = a1 + ALPHA * x
-    y = a2 - ALPHA * x
-    a1 + ALPHA * x = a2 - ALPHA * x
-    ALPHA * x = a2 - a1 - ALPHA * x
-    x = (a2 - a1) / ALPHA - x
-    2x = (a2 - a1) / ALPHA
-    */
-
-/*
-    x = (a2 - a1) / (2 * ALPHA)
-    y = a1 + ALPHA * x
-    */
