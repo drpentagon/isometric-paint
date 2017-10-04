e253e6dbd3bf3fb8c6e5180da5fa359fe9e42fb2
Martin Svärd <martinsvard@me.com>
2017-10-04 21:52:18 +0200

    Better data representation of painting

--- src/layer.js
+++ src/layer.js
@@ -5,28 +5,41 @@ export default class Layer {
   constructor (name) {
     this.gh = new GraphicsHandler(CONTAINER)
     this.gh.lineWidth = 1
-    this.triangles = localStorage.painting ? JSON.parse(localStorage.painting) : []
+    this.triangles = localStorage.painting ? JSON.parse(localStorage.painting) : {}
     this.render()
   }
 
   addTriangle (a1, a2, right, color) {
-    this.triangles.push({
+    this.triangles[`${a1}_${a2}_${right}`] = {
       a1: a1,
       a2: a2,
       right: right,
       color: color
-    })
+    }
 
     localStorage.painting = JSON.stringify(this.triangles)
   }
 
+  removeTriangle (a1, a2, right) {
+    delete this.triangles[`${a1}_${a2}_${right}`]
+  }
+
   render () {
     this.gh.clearCanvas()
-    this.triangles.forEach((t) => {
+    Object.keys(this.triangles).map((t) => {
+      const {a1, a2, right, color} = this.triangles[t]
+
+      this.gh.fillStyle = color
+      this.gh.strokeStyle = color
+      this.gh.drawTriangle(a1, a2, right)
+    })
+    /*
+    this.triangles.map((t) => {
       const {a1, a2, right, color} = t
       this.gh.fillStyle = color
       this.gh.strokeStyle = color
       this.gh.drawTriangle(a1, a2, right)
     })
+    */
   }
 }
